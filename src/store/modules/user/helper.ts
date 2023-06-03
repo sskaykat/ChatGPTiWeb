36df1187b97a3f23085a226d1b5ba8f3bcdd674d
sskaykat <88110259+sskaykat@users.noreply.github.com>
2023-06-03 15:08:46 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://xinghuajiu.tk/file/58b507968698ce2269c73.jpg',
+      name: '一碗桃花潭水',
+      description: '',
     },
   }
 }
